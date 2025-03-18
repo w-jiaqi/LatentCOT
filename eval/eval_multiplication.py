c40172da32f17814b6a4fec20797ec3daa6a26d0
enyoukai <an.joseph0001@gmail.com>
2025-03-18 11:41:25 -0700

    update

--- eval/eval_multiplication.py
+++ eval/eval_multiplication.py
@@ -1,23 +1,24 @@
 import torch
-from transformers import (
-    AutoTokenizer,
-    AutoModelForCausalLM,
-	pipeline
-)
+from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 from peft import PeftModel
 import argparse
 
 from tqdm.auto import tqdm
 
 import sys, os
-sys.path.insert(0, os.path.abspath('.')) # hack for imports
+
+sys.path.insert(0, os.path.abspath("."))  # hack for imports
 
 from data import dataset
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-m", "--base_model", type=str, default="meta-llama/Llama-3.2-1B-Instruct")
+parser.add_argument(
+    "-m", "--base_model", type=str, default="meta-llama/Llama-3.2-1B-Instruct"
+)
 parser.add_argument("-c", "--finetune_dir", type=str, default=None)
-parser.add_argument("-d", "--dataset", choices=['4x4', '5x5', 'all'], type=str, required=True)
+parser.add_argument(
+    "-d", "--dataset", choices=["4x4", "5x5", "all"], type=str, required=True
+)
 parser.add_argument("--device", default="cuda")
 parser.add_argument("--dtype", default=torch.bfloat16)
 parser.add_argument("--log_dir", default="eval/logs/multiplication")
@@ -29,64 +30,65 @@ model = None
 tokenizer = None
 
 if args.finetune_dir == None:
-	print("USING BASE MODEL")
-	model = AutoModelForCausalLM.from_pretrained(args.base_model)
-	
-	tokenizer = AutoTokenizer.from_pretrained(args.base_model)
+    print("USING BASE MODEL")
+    model = AutoModelForCausalLM.from_pretrained(args.base_model)
+
+    tokenizer = AutoTokenizer.from_pretrained(args.base_model)
 
 else:
-	base_model = AutoModelForCausalLM.from_pretrained(args.base_model)
+    base_model = AutoModelForCausalLM.from_pretrained(args.base_model)
 
-	model = PeftModel.from_pretrained(base_model, args.finetune_dir)
-	model = model.merge_and_unload()
+    model = PeftModel.from_pretrained(base_model, args.finetune_dir)
+    model = model.merge_and_unload()
 
-	tokenizer = AutoTokenizer.from_pretrained(args.finetune_dir)
+    tokenizer = AutoTokenizer.from_pretrained(args.finetune_dir)
 
 generator = pipeline(
-	"text-generation",
-	model=model,
-	tokenizer=tokenizer,
-	torch_dtype=args.dtype,
-	device_map=args.device
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer,
+    torch_dtype=args.dtype,
+    device_map=args.device,
 )
 
 ds = None
 
 if args.dataset == "4x4":
-	ds = dataset.get_4x4_multiplication_dataset(eval_only=True)
+    ds = dataset.get_4x4_multiplication_dataset(eval_only=True)
 
 pb = tqdm(range(len(ds)))
 
 correct = 0
 
 for idx, example in enumerate(ds):
-	def get_ans_from_response(response):
-		answer = response.split("####")[-1].strip().replace(" ", "")[::-1] # reversing string
 
-		try:
-			return int(answer)
-		except ValueError:
-			return answer
+    def get_ans_from_response(response):
+        answer = (
+            response.split("####")[-1].strip().replace(" ", "")[::-1]
+        )  # reversing string
 
-	pred_string = generator(example["prompt"], max_new_tokens=128)[0]['generated_text'][-1]['content']
+        try:
+            return int(answer)
+        except ValueError:
+            return answer
 
-	pred_ans = get_ans_from_response(pred_string)
-	true_ans = get_ans_from_response(example['completion'][0]['content'])
+    pred_string = generator(example[0:2], max_new_tokens=128)[0]["generated_text"][-1][
+        "content"
+    ]
 
-	if pred_ans == true_ans:
-		correct += 1
+    pred_ans = get_ans_from_response(pred_string)
+    true_ans = get_ans_from_response(example["completion"][0]["content"])
 
-	accuracy = (correct / (idx + 1)) * 100
+    if pred_ans == true_ans:
+        correct += 1
 
-	print(pred_string)
+    accuracy = (correct / (idx + 1)) * 100
 
-	pb.set_description(f"{accuracy}%")
-	pb.update(1)
+    print(pred_string)
 
-	# if idx % args.logging_steps == 0:
-	# 	with open(f"{args.log_dir}/{args.base_model}-{args.finetune_dir}-{args.dataset}.log", "a") as f:
-	# 		f.write(f"\nAccuracy: {accuracy}%")
+    pb.set_description(f"{accuracy}%")
+    pb.update(1)
 
-
-
-
+    # if idx % args.logging_steps == 0:
+    # 	with open(f"{args.log_dir}/{args.base_model}-{args.finetune_dir}-{args.dataset}.log", "a") as f:
+    # 		f.write(f"\nAccuracy: {accuracy}%")
