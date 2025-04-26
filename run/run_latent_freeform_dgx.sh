7e0d586ebbccd6570d6289ac14fddb6a9429f561
inkitori <an.joseph0001@gmail.com>
2025-04-26 04:01:11 -0700

    fixing dgx config

--- run/run_latent_freeform_dgx.sh
+++ run/run_latent_freeform_dgx.sh
@@ -5,10 +5,8 @@
 #SBATCH --job-name=latent_freeform
 #SBATCH --output=/lustre/fs0/scratch/pkeung/multiplication/LatentCOT/logs/%x_%j/log.out
 
-CONFIG_PATH = $1
-
 srun --container-image=/lustre/fs0/scratch/pkeung/squash/nvidia+pytorch+24.10-py3.sqsh \
      --container-mounts=/lustre/fs0 \
      bash -c "cd /lustre/fs0/scratch/pkeung/multiplication/LatentCOT && \
                source env/bin/activate && \
-               python sft/latent_cot_grpo.py -c ${CONFIG_PATH}"
+               python sft/latent_cot_grpo.py -c $1"
